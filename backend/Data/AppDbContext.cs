a5ac3305ad5738044cc1aadb349ed67481d5b38d
btwamibatman <146748619+btwamibatman@users.noreply.github.com>
2026-03-14 01:30:11 +0500

    Refactor namespaces and remove unused code in Data layer

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -1,23 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using Data.Entities;
 
-namespace Api.Data;
+namespace Data;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    // Example table:
     public DbSet<User> Users => Set<User>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().ToTable("users");
     }
-}
-
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public bool IsActive { get; set; }
 }
